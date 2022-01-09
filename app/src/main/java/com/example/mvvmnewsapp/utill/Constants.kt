465341a56497aa163a189735845a749162128c31
juliandocev <j.docev@gmail.com>
2022-01-09 03:56:58 +0100

    Retrofit and DAO added

---
+++ app/src/main/java/com/example/mvvmnewsapp/utill/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.mvvmnewsapp.utill
+
+class Constants {
+
+    companion object {
+        const val API_KEY = "22d7a6b65a05440eba9579d04c3bc75c"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
